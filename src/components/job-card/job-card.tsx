749b179946c7885d22df46b9c9d7b51e8811f2fb
Klecianny Melo <klecianny.melo@quorum.us>
2026-05-02 19:18:59 -0300

    :sparkles: job card

--- src/components/job-card/job-card.tsx
+++ src/components/job-card/job-card.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
+import { Link, useLocation } from "@builder.io/qwik-city";
 import { LuArrowRight } from "@qwikest/icons/lucide";
+import { TAG_COLORS } from "../../data/positions";
 
 type JobCardProps = {
   slug: string;
@@ -10,15 +11,10 @@ type JobCardProps = {
   description: string;
 };
 
-const TAG_COLORS: Record<string, string> = {
-  Product: "bg-[#27115f] text-[#bdb4fe] border-[1.5px] border-[#4a1fb8]",
-  Engineering: "bg-[#27115f] text-[#bdb4fe] border-[1.5px] border-[#4a1fb8]",
-  Marketing: "bg-[#4e0d30] text-[#faa7e0] border-[1.5px] border-[#9e165f]",
-  "AI & Data": "bg-[#4e1d09] text-[#fec84b] border-[1.5px] border-[#93370d]",
-};
-
 export const JobCard = component$<JobCardProps>(
   ({ slug, title, location, tag, description }) => {
+    const loc = useLocation();
+    const search = loc.url.search;
     const tagClass = TAG_COLORS[tag] ?? "border text-white";
     return (
       <article class="group relative flex h-62 flex-col gap-3 rounded-2xl border-2 border-transparent bg-[#13161b] p-8 transition-colors hover:border-[#4c457f] hover:bg-linear-to-r hover:from-[#211e32] hover:to-[#10121b] hover:to-70%">
@@ -37,7 +33,7 @@ export const JobCard = component$<JobCardProps>(
           {description}
         </p>
         <Link
-          href={`/jobs/${slug}`}
+          href={`/jobs/${slug}${search}`}
           class="flex h-9 w-[135.09px] items-center justify-center gap-1.5 self-start rounded-full px-3 py-2 text-[#94979c] transition-colors group-hover:bg-[#21262e] after:absolute after:inset-0 after:content-['']"
         >
           <span class="font-ui px-0.5 text-[14px] font-semibold">View Details</span>
