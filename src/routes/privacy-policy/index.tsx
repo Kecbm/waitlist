3ad8458fbdcf6f03b08fe7c954525cfe1376f3a4
Klecianny Melo <klecianny.melo@quorum.us>
2026-05-02 17:03:18 -0300

    :sparkles: main in privacy police

--- src/routes/privacy-policy/index.tsx
+++ src/routes/privacy-policy/index.tsx
@@ -2,14 +2,15 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Header } from "../../components/header/header";
 import { Footer } from "../../components/footer/footer";
+import { PageMain } from "../../components/page-main/page-main";
 
 export default component$(() => {
   return (
     <div class="flex min-h-screen flex-col">
       <Header />
-      <main class="flex flex-1 items-center justify-center px-4 py-8 sm:px-8">
+      <PageMain class="items-center justify-center">
         <h1 class="text-2xl font-medium sm:text-3xl">Privacy Policy</h1>
-      </main>
+      </PageMain>
       <Footer />
     </div>
   );
